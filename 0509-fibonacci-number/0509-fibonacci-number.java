2a210a45a96c0719522c1188f1a8c9edf8891236
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-04 11:06:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (12.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -15,15 +15,30 @@
 // }
 
 //bottom up
+// class Solution{
+//     public int fib(int n){
+//         if(n<=1)return n;
+//         int dp[]=new int[n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+//space optimization
 class Solution{
     public int fib(int n){
-        if(n<=1)return n;
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
+        if(n==0)return 0;
+        int a=0;
+        int b=1;
+        int s=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            s=a+b;
+            a=b;
+            b=s;
         }
-        return dp[n];
+        return b;
     }
 }
