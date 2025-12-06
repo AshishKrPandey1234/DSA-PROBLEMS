0bea5614e1e3b34af72621fb593980f42c4829b9
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-07 03:31:01 +0530

    Time: 1 ms (99.9%), Space: 46.6 MB (59.3%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer>set=new HashSet<>();
-        //since every element occur twice so we will keep one sum variable where sum of array will be there we will add unique ele in set now we will calculate the sum of set *2, totalsum-setsum will be the answer
-        int sum=0;
-        int setSum=0;
+        //bitwise xor operator when we do xor of two same number than we got 0 
+        //if 0 xor unique=uniue is the ans
+        int res=0;
         for(int num:nums){
-            sum+=num;
-            if(!set.contains(num)){
-                set.add(num);
-                setSum+=num;
-            }
+            res=res^num;
         }
-        return 2*setSum-sum;
+        return res;
     }
 }
