2bdbae008ac293ad6d68f8ce42cd8361ba98e54b
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-10 09:41:04 +0530

    Time: 0 ms (100%), Space: 42.8 MB (70.38%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        //as question want that alice should win whoever will start first and play optimaaly than first person will only win
+        return true;
+        
+    }
+}
