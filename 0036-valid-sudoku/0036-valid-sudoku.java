88d6b8b0f317d01b1be9052a2cccb7e1a1121ca9
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-18 10:48:19 +0530

    Time: 1 ms (99.75%), Space: 46.7 MB (20.88%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isValid(char board[][],int row,int col,char num){
         //checking for row
-        for(int j=0;j<9;j++){
+        for(int j=0,i=0;j<9;j++,i++){
             if(board[row][j]==num)return false;
-        }
-        //checking for column
-        for(int i=0;i<9;i++){
             if(board[i][col]==num)return false;
         }
+        //checking for column
+        // for(int i=0;i<9;i++){
+        //     if(board[i][col]==num)return false;
+        // }
 
         //checking for 3*3 Grid
         int sRow=(row/3)*3;//this will find the starting row and starting col  of subgrid trick is dividing current row by 3 than multiplying by 3
@@ -23,7 +24,7 @@ class Solution {
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.')continue;//it means pahle se row khali hai than continue jo khai hai usee check nahi karenge
-                ;
+                
                 char num=board[i][j];
                 board[i][j]='.';
                 if(!isValid(board,i,j,num))return false;
