530f87c1c4404b9a6715d2fef8b51b5f6527874b
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-21 20:47:35 +0530

    Time: 0 ms (100%), Space: 42.3 MB (7.96%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -14,31 +14,31 @@
 // }
 
 //bottom up: tabulation 
-// class Solution{
-//     public int fib(int n){
-//         if(n<=1)return n;
-//         int dp[]=new int[n+1];
-//         dp[0]=0;
-//         dp[1]=1;
-//         for(int i=2;i<=n;i++){
-//             dp[i]=dp[i-1]+dp[i-2];
-//         }
-//         return dp[n];
-//     }
-// }
+class Solution{
+    public int fib(int n){
+        if(n<=1)return n;
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
 
 
 //space optimization
-class Solution{
-    public int fib(int n){
-        if(n==0)return 0;
-        int a=0;
-        int b=1;
-        for(int i=2;i<=n;i++){
-            int sum=a+b;
-            a=b;
-            b=sum;
-        }
-        return b;
-    }
-}
+// class Solution{
+//     public int fib(int n){
+//         if(n==0)return 0;
+//         int a=0;
+//         int b=1;
+//         for(int i=2;i<=n;i++){
+//             int sum=a+b;
+//             a=b;
+//             b=sum;
+//         }
+//         return b;
+//     }
+// }
