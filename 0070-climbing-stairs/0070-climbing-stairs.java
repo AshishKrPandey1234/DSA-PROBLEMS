28d50232c2bdb20a81e33fb9b2d04748d5303465
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-21 20:38:48 +0530

    Time: 0 ms (100%), Space: 42.1 MB (39.19%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,71 @@
-class Solution {
-    private int solve(int dp[],int n){
-        if(n<=1)return 1;
+//recursion
+// class Solution{
+//     public int climbStairs(int n){
+//         if(n<=1)return 1;
+//         return climbStairs(n-1)+climbStairs(n-2);
+//     }
+// }
+
+//memorization top down approach
+class Solution{
+    private int solve(int n,int dp[]){
+        if(n<=1)return 1;//that is 0 th step se 0th step jane mai bhi 1 step lagega and 1 se bhi same
         if(dp[n]!=-1)return dp[n];
-        dp[n]=solve(dp,n-1)+solve(dp,n-2);
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
         return dp[n];
     }
-    public int climbStairs(int n) {
+
+    public int climbStairs(int n){
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        return solve(dp,n);
+        return solve(n,dp);
     }
-    
 }
+
+
+
+
+//top down
+// class Solution {
+//     private int solve(int dp[],int n){
+//         if(n<=1)return 1;
+//         if(dp[n]!=-1)return dp[n];
+//         dp[n]=solve(dp,n-1)+solve(dp,n-2);
+//         return dp[n];
+//     }
+//     public int climbStairs(int n) {
+//         int dp[]=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return solve(dp,n);
+//     }
+    
+// }
+
+//bottom up approach
+// class Solution{
+//     public int climbStairs(int n){
+//         int dp[]=new int[n+1];
+//         dp[0]=1;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+
+//     }
+// }
+
+//space optimization that is using loop
+// class Solution{
+//     public int climbStairs(int n){
+//         int a=1;
+//         int b=1;
+//         int s=0;
+//         for(int i=2;i<=n;i++){
+//             s=a+b;
+//             a=b;
+//             b=s;
+//         }
+//         return b;
+//     }
+// }
