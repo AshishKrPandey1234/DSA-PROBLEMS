3ed6b4b7d263a1ef1c48f23dca97ee4a35204fe4
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-21 20:45:14 +0530

    Time: 0 ms (100%), Space: 42.2 MB (20.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,17 @@
 //top down : memoization
-class Solution {
-    private int solve(int dp[],int n){
-        if(n<=1)return n;
-        if(dp[n]!=-1)return dp[n];
-        dp[n]=solve(dp,n-1)+solve(dp,n-2);
-        return dp[n];
-    }
-    public int fib(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(dp,n);
-    }
-}
+// class Solution {
+//     private int solve(int dp[],int n){
+//         if(n<=1)return n;
+//         if(dp[n]!=-1)return dp[n];
+//         dp[n]=solve(dp,n-1)+solve(dp,n-2);
+//         return dp[n];
+//     }
+//     public int fib(int n) {
+//         int dp[]=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return solve(dp,n);
+//     }
+// }
 
 //bottom up: tabulation 
 // class Solution{
@@ -29,17 +29,16 @@ class Solution {
 
 
 //space optimization
-// class Solution{
-//     public int fib(int n){
-//         if(n==0)return 0;
-//         int a=0;
-//         int b=1;
-//         int s=0;
-//         for(int i=2;i<=n;i++){
-//             s=a+b;
-//             a=b;
-//             b=s;
-//         }
-//         return b;
-//     }
-// }
+class Solution{
+    public int fib(int n){
+        if(n==0)return 0;
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return b;
+    }
+}
