7035b397fcaba6ad2bb2ad828393daa13c07b031
AshishKrPandey1234 <akpandey369936@gmail.com>
2025-12-30 09:47:56 +0530

    Time: 1 ms (99.82%), Space: 51 MB (48.83%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        
-        int n=nums.length;
-        int count=0;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==goal)count++;
+    private int helper(int[] nums, int goal){
+        if(goal<0)return 0;
+        int l=0,r=0,n=nums.length,sum=0,count=0;
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal ){
+                sum-=nums[l];
+                l++;
             }
+            count+=(r-l+1);//this will calculate for sum<=goal/as we know we have to find sum==goal counts but what we will do we will find sum<=goal and sum<=goal-1 we will do substract them and get the result as we know sum<=goal-sum<goal->sum==goal
+            r++;
         }
         return count;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return helper(nums,goal)-helper(nums,goal-1);
     }
 }
