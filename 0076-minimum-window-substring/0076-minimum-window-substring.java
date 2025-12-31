7a1bb7d5b14f8d5d9180f16e5786746f1a44a0f6
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-01 00:16:39 +0530

    Time: 2 ms (99.77%), Space: 45.7 MB (75.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,34 +1,36 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int m=t.length();
-        int n=s.length();
-        int freq[]=new int[256];
-        int l=0,r=0,minLen=Integer.MAX_VALUE;
-        int sIdx=-1;
+        int m = t.length();
+        int n = s.length();
+        int freq[] = new int[256];
+        int l = 0, r = 0, minLen = Integer.MAX_VALUE;
+        int sIdx = -1;
         //initialising the frequency of text in freq array
-        for(int i=0;i<m;i++){
+        for (int i = 0; i < m; i++) {
             freq[t.charAt(i)]++;
         }
-        int count=0;
-        while(r<n){
-            if(freq[s.charAt(r)]>0){
-                count=count+1;//since freq>0 for s.charAt(r) it means in text it must be ther becuase till now only text is inside the freqarray
+        int count = 0;
+        while (r < n) {
+            if (freq[s.charAt(r)] > 0) {
+                count = count + 1;//since freq>0 for s.charAt(r) it means in text it must be ther becuase till now only text is inside the freqarray
             }
             freq[s.charAt(r)]--;//once count is calculated now freq will be decreased since it is already compared with thta character
-            while(count==m){
-                if(r-l+1<minLen){
-                    minLen=r-l+1;
-                    sIndex=l;
+            while (count == m) {
+                if (r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    sIdx = l;
                 }
-                freq[s.charAt(l)]--;
+                freq[s.charAt(l)]++;
+                if (freq[s.charAt(l)] > 0) {//it means not present in the text
+                    count = count - 1;
+                }
+                l++;
             }
 
-            if(freq[s.charAt(l)]>0){//it means not present in the text
-            count=count-1;
-            }
-            r=r-1;
+            
+            r++;
         }
-        return sIdx==-1?"":s.subString(sIdx,minLen);
-        
+        return sIdx == -1 ? "" : s.substring(sIdx, sIdx + minLen);
+
     }
 }
