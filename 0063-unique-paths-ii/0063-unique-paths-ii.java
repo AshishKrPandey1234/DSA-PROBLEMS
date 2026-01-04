c1632b16ece78821e8674cc5591eec468d494418
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-05 00:11:24 +0530

    Time: 0 ms (100%), Space: 43.9 MB (10.29%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,25 +1,26 @@
 class Solution {
-    private int solve(int[][] obstacleGrid,int i,int j,int dp[][]){
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1)return dp[i][j]=0;//we did check i>=0 && j>=0 because we should not check out of bound things also if (0,0)cell is 1 at that time we should not return 1 istead 0 will be returned so we can call this defensive coding practise
-        if(i==0 && j==0)return dp[i][j]= 1;
-        if(i<0 || j<0)return 0;
-
-        if(dp[i][j]!=-1)return dp[i][j];//this is done to resue the value which is alrady computed no need to calculate again;
-        
-        int up=solve(obstacleGrid,i-1,j,dp);
-        int left=solve(obstacleGrid,i,j-1,dp);
-        return dp[i][j]=up+left;
-
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+        int prev[] = new int[n];
+        for (int i = 0; i < m; i++) {
+            int curr[] = new int[n];
+            for (int j = 0; j < n; j++) {
+                if(obstacleGrid[i][j]==1)curr[j]=0;
+                else if (i == 0 && j == 0) {
+                    curr[j] = 1;
+                } else {
+                    int up = 0;
+                    int left = 0;
+                    if (i > 0)
+                        up = prev[j];
+                    if (j > 0)
+                        left = curr[j - 1];
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
         }
-        return solve(obstacleGrid,m-1,n-1,dp);
-
-        
+        return prev[n - 1];
     }
 }
