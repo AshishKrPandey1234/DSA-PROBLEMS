9a2a93499bb33be336f81942646b017256fcee00
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-05 22:55:51 +0530

    Time: 6 ms (10.54%), Space: 50.2 MB (52.92%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,26 +1,23 @@
 class Solution {
-    private int solve(int dp[][],int grid[][],int i,int j){
-        
-        if(i==0 && j==0)return dp[i][j]=grid[0][0];
-        if(i<0 || j<0)return (int) 1e9;//Java treats 1e9 as a double by default, not an int
-        // We use 1e9 instead of Integer.MAX_VALUE to avoid integer overflow.
-// Adding any positive value to Integer.MAX_VALUE causes overflow and wraps to Integer.MIN_VALUE (negative),
-// which can break min/max comparisons and produce incorrect results.
-        if(dp[i][j]!=-1)return dp[i][j];
-
-
-        int up=grid[i][j]+solve(dp,grid,i-1,j);
-        int left=grid[i][j]+solve(dp,grid,i,j-1);
-
-        return dp[i][j]=Math.min(up,left);
-    }
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int[m][n];
+        
         for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)dp[i][j]=grid[i][j];
+                //if(i<0 || j<0)return (int)1e9;
+                else{
+                   int up=(i>0)?grid[i][j]+dp[i-1][j]:(int)1e9;
+                   int left=(j>0)?grid[i][j]+dp[i][j-1]:(int)1e9;
+
+                    dp[i][j]=Math.min(up,left);
+                }
+
+            }
         }
-        return solve(dp,grid,m-1,n-1);
+        return dp[m-1][n-1];
+        
     }
 }
