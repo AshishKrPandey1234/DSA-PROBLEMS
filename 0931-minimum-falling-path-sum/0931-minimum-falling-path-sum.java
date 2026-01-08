9905dadbaec6f89c2e6e3bb9de85cc6a705341f1
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-08 10:55:04 +0530

    Time: 6 ms (67.29%), Space: 47.1 MB (67.61%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,28 +1,28 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        //Tabulation
+        //space optimization
         int m=matrix.length;
         int n=matrix[0].length;
-        int dp[][]=new int[m][n];
-        //Any falling path must start from row 0 ,So dp[0][col] = matrix[0][col]
+        int prev[]=new int[n];
+        //first row  will be same 
         for(int col=0;col<n;col++){
-            dp[0][col]=matrix[0][col];
+            prev[col]=matrix[0][col];
         }
         for(int row=1;row<m;row++){
+            int curr[]=new int[n];
             for(int col=0;col<n;col++){
-                int down=dp[row-1][col];
-                int leftdiag=(col>0)?dp[row-1][col-1]:(int)1e9;
-                int rightdiag=(col<n-1)?dp[row-1][col+1]:(int)1e9;
-                dp[row][col]=matrix[row][col]+Math.min(down,Math.min(leftdiag,rightdiag));
+                int down=prev[col];//pichle wale row ka col
+                int leftdiag=(col>0)?prev[col-1]:(int)1e9;
+                int rightdiag=(col<m-1)?prev[col+1]:(int)1e9;
+                curr[col]=matrix[row][col]+Math.min(down,Math.min(leftdiag,rightdiag));
             }
+            prev=curr;
         }
-
-        //now pick the min from the last row from the dp table as sum is done and stored in bottom so now we will print minimum one
         int minSum=(int)1e9;
-        for(int col=0;col<n;col++){
-            minSum=Math.min(minSum,dp[m-1][col]);
+        for(int val:prev){
+            minSum=Math.min(minSum,val);
+
         }
         return minSum;
-        
     }
 }
