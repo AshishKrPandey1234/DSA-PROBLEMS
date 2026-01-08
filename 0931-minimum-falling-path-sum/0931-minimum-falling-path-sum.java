bf782e3768c327f03549b89559503e4bbeadf0a9
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-08 09:35:48 +0530

    Time: 8 ms (31.65%), Space: 47.5 MB (19.48%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,27 +1,28 @@
 class Solution {
-    private int solve(int matrix[][],int i,int j,int dp[][]){
-        int m=matrix.length;
-        int n=matrix[0].length;
-        if(j>=n || j<0)return (int)1e9;
-        if(i==m-1)return matrix[i][j];
-        if(dp[i][j]!=(int)1e9)return dp[i][j];
-        int down=matrix[i][j]+solve(matrix,i+1,j,dp);
-        int leftDiag=matrix[i][j]+solve(matrix,i+1,j-1,dp);
-        int rightDiag=matrix[i][j]+solve(matrix,i+1,j+1,dp);
-        return dp[i][j]= Math.min(down,Math.min(leftDiag,rightDiag));
-
-    }
     public int minFallingPathSum(int[][] matrix) {
+        //Tabulation
         int m=matrix.length;
         int n=matrix[0].length;
-        int ans=(int)1e9;
         int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],(int)1e9);
+        //Any falling path must start from row 0 ,So dp[0][col] = matrix[0][col]
+        for(int col=0;col<n;col++){
+            dp[0][col]=matrix[0][col];
         }
-        for(int j=0;j<n;j++){
-            ans=Math.min(ans,solve(matrix,0,j,dp));
+        for(int row=1;row<m;row++){
+            for(int col=0;col<n;col++){
+                int down=dp[row-1][col];
+                int leftdiag=(col>0)?dp[row-1][col-1]:(int)1e9;
+                int rightdiag=(col<n-1)?dp[row-1][col+1]:(int)1e9;
+                dp[row][col]=matrix[row][col]+Math.min(down,Math.min(leftdiag,rightdiag));
+            }
         }
-        return ans;
+
+        //now pick the min from the last row from the dp table as sum is done and stored in bottom so now we will print minimum one
+        int minSum=(int)1e9;
+        for(int col=0;col<n;col++){
+            minSum=Math.min(minSum,dp[m-1][col]);
+        }
+        return minSum;
+        
     }
 }
