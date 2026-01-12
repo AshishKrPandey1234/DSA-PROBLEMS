35795067cf2be977090649d3c278a46c5276693f
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-13 00:18:57 +0530

    Time: 32 ms (26.07%), Space: 46.6 MB (36.44%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,38 +1,34 @@
 class Solution {
-    private int solve(int grid[][],int i,int j1,int j2,int dp[][][]){
+    public int cherryPickup(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        //check out of boundary condition
-        if(j1<0||j1>=n||j2<0||j2>=n)return (int)(-1e9);
-        //base case if we reach the last row
-        if(i==m-1){
-            return dp[i][j1][j2]= (j1==j2)?grid[i][j1]:grid[i][j1]+grid[i][j2];
-        }
-        //check if already computed it than return it
-        if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
-        int curr=(j1==j2)?grid[i][j1]:grid[i][j1]+grid[i][j2];
-        int max=(int)(-1e9);
-        for(int dj1=-1;dj1<=1;dj1++){
-            for(int dj2=-1;dj2<=1;dj2++){
-                int ans=curr+solve(grid,i+1,j1+dj1,j2+dj2,dp);
-                max=Math.max(max,ans);
+        int dp[][][]=new int[m][n][n];
+        for(int j1=0;j1<n;j1++){
+            for(int j2=0;j2<n;j2++){
+                dp[m-1][j1][j2]=(j1==j2)?grid[m-1][j1]:grid[m-1][j1]+grid[m-1][j2];
             }
         }
-        return dp[i][j1][j2]=max;
-        
-    }
-    public int cherryPickup(int[][] grid) {
-        //since three variable changing i,j1,j2 since i is changing by+1 each time for both robert at a time but robert1 column change is independent of robert 2 column change
-        int m=grid.length;//row length
-        int n=grid[0].length;//column length;
-        int dp[][][]=new int[m][n][n];
-        for(int i=0;i<m;i++){
+        //fill the table by bottom up approach from base case to its initial position
+        for(int i=m-2;i>=0;i--){
             for(int j1=0;j1<n;j1++){
                 for(int j2=0;j2<n;j2++){
-                    dp[i][j1][j2]=-1;
+                    int max=(int)(-1e9);
+                    int curr=(j1==j2)?grid[i][j1]:grid[i][j1]+grid[i][j2];
+                    //try out all the 9 move
+                    for(int dj1=-1;dj1<=1;dj1++){
+                        for(int dj2=-1;dj2<=1;dj2++){
+                            int newj1=j1+dj1;
+                            int newj2=j2+dj2;
+                            if(newj1>=0 && newj1<n && newj2>=0 && newj2<n){
+                                max=Math.max(max,curr+dp[i+1][newj1][newj2]);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2]=max;
                 }
             }
         }
-        return solve(grid,0,0,n-1,dp);
+        return dp[0][0][n-1];//that is robert1 and robert2 starting pos is 0 but ending point of robert2 is 0,n-1
+        
     }
 }
