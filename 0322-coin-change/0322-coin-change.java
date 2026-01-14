f4179616399897d621ba64c86ba738bebec6824f
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-14 20:12:09 +0530

    Time: 33 ms (17.95%), Space: 47.7 MB (9.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,8 +1,8 @@
 class Solution {
-    private int solve(int coins[],int idx,int amount,Integer dp[][]){
+    private int solve(int coins[],int idx,int amount,int dp[][]){
         if(amount==0)return 0;
         if(idx<0)return (int)1e9;
-        if(dp[idx][amount]!=null)return dp[idx][amount];
+        if(dp[idx][amount]!=-2)return dp[idx][amount];
         int notPick=solve(coins,idx-1,amount,dp);//while skipping there will be change in index 
         //but while picking there would be no change in index as there is no change in index
         int pick=(int)1e9;
@@ -13,7 +13,8 @@ class Solution {
     }
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        Integer dp[][]=new Integer[n][amount+1];
+        int dp[][]=new int[n][amount+1];
+        for(int row[]:dp)Arrays.fill(row,-2);
         int ans=solve(coins,coins.length-1,amount,dp);
         return ans>=1e9?-1:ans;
     }
