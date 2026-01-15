3e74daa744f6576a303bcf5b39ff13d4ec9c15ca
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-01-16 01:04:19 +0530

    Time: 2 ms (67.01%), Space: 45.3 MB (5.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,16 +1,18 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int i=0,j=0;
-        while(i<s.length() && j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
-                i++;//match kara toh i badhega
-            }
-            //match kare toh i and j badhega
-            //warna sirf j abdhega menas j toh badhega hi at any cost
-            j++;
-
-            //since length of s string can be<=t.length
+    Boolean dp[][];
+    private boolean solve(int i,int j,String s,String t){
+        if(i==s.length())return true;
+        if(j==t.length())return false;
+        if(dp[i][j]!=null)return dp[i][j];
+        if(s.charAt(i)==t.charAt(j)){
+            dp[i][j]=solve(i+1,j+1,s,t);
+        }else{
+            dp[i][j]=solve(i,j+1,s,t);
         }
-        return i==s.length();
+        return dp[i][j];
+    }
+    public boolean isSubsequence(String s, String t) {
+        dp=new Boolean[s.length()+1][t.length()+1];
+        return solve(0,0,s,t);
     }
 }
