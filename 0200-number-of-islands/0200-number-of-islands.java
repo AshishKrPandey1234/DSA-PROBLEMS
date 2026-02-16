b4ed14129b5eaccc838f5473c11e4b8fc7c27421
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-02-16 16:20:34 +0530

    Time: 2 ms (99.78%), Space: 52.4 MB (45.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,28 +1,29 @@
 class Solution {
-    private void dfs(char vis[][],int i,int j){
+    private void DFS(char vis[][],int i,int j){
         int n=vis.length;
         int m=vis[0].length;
-        if(i<0 || i>=n || j<0 || j>=m || vis[i][j]!='1')return;
-        vis[i][j]='0';
-        dfs(vis,i+1,j);
-        dfs(vis,i-1,j);
-        dfs(vis,i,j+1);
-        dfs(vis,i,j-1);
+        if(i<0 || i>=n || j<0 || j>=m || vis[i][j]!='1'){
+            return;
+        }
+        vis[i][j]=0;//it means visited
+        DFS(vis,i+1,j);
+        DFS(vis,i-1,j);
+        DFS(vis,i,j-1);
+        DFS(vis,i,j+1);
     }
     public int numIslands(char[][] grid) {
         int rows=grid.length;
         int cols=grid[0].length;
-        int count=0;
+        int isLandCount=0;
         for(int r=0;r<rows;r++){
             for(int c=0;c<cols;c++){
                 if(grid[r][c]=='1'){
-                    count++;
-                    dfs(grid,r,c);
-
+                    isLandCount++;
+                    DFS(grid,r,c);
                 }
             }
         }
-        return count;
+        return isLandCount;
         
     }
 }
