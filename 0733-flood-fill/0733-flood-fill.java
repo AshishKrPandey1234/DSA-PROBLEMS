7f73c6ce5107e998d28f23051dceceb9e87c9936
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-02-18 09:24:18 +0530

    Time: 0 ms (100%), Space: 47 MB (43.81%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,18 @@
 class Solution {
-    private void dfs(int image[][],int row,int col,int currColor,int newColor,boolean vis[][]){
-        if(row<0||row>=image.length||col<0||col>=image[0].length||image[row][col]!=currColor||vis[row][col]==true){
+    private void dfs(int image[][],int row,int col,int currColor,int newColor){
+        if(row<0||row>=image.length||col<0||col>=image[0].length||image[row][col]!=currColor||image[row][col]==newColor){
             return;
         }
         image[row][col]=newColor;
-        vis[row][col]=true;
         int adj[][]={{row-1,col},{row,col-1},{row+1,col},{row,col+1}};
         for(int nbr[]:adj){
-            dfs(image,nbr[0],nbr[1],currColor,newColor,vis);
+            dfs(image,nbr[0],nbr[1],currColor,newColor);
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int rows=image.length;
         int cols=image[0].length;
-        boolean vis[][]=new boolean[rows][cols];
-        dfs(image,sr,sc,image[sr][sc],color,vis);
+        dfs(image,sr,sc,image[sr][sc],color);
         return image;
     }
 }
