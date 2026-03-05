cbe6d58676b777990d2c87da67388038c00344cc
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-03-05 17:35:58 +0530

    Time: 5 ms (53.53%), Space: 43.9 MB (45.61%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -4,12 +4,13 @@ class Solution {
         int start1=0;//10101010101010....
         for(int i=0;i<s.length();i++){
             if(i%2==0){//even index
-                if(s.charAt(i)=='0')start0++;
-                else if(s.charAt(i)=='1')start1++;
-            }
-            else if(i%2==1){
+            //The counter should increase when the character does NOT match the expected pattern, because that means one operation (flip) is needed.
                 if(s.charAt(i)=='0')start1++;
                 else if(s.charAt(i)=='1')start0++;
+            }
+            else if(i%2==1){
+                if(s.charAt(i)=='0')start0++;
+                else if(s.charAt(i)=='1')start1++;
             }
         }
         return Math.min(start0,start1);
