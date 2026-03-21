7ba29b97fbe9a87f9d17a1733e031ca3a97807f9
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-03-21 23:36:01 +0530

    Time: 16 ms (58.28%), Space: 47.4 MB (89.93%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,56 +1,51 @@
-class Tuple {
+class Tuple{
     int first;
     int second;
     int third;
-
-    Tuple(int _first, int _second, int _third) {
-        this.first = _first;
-        this.second = _second;
-        this.third = _third;
+    Tuple(int _first,int _second,int _third){
+        this.first=_first;
+        this.second=_second;
+        this.third=_third;
     }
 }
-
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.length;
-        if(grid[0][0]==1)return -1;
-        if (n == 1){
+        int n=grid.length;
+        if(grid[0][0]==1)return -1;//not possible
+        if(n==1){
             return 1;
         }
-            
-        int dist[][] = new int[n][n];
-
-        for (int row[] : dist) {
-            Arrays.fill(row, (int) 1e9);
+        int dist[][]=new int[n][n];
+        for(int row[]:dist){
+            Arrays.fill(row,(int)1e9);
         }
-        Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(1, 0, 0));//first 0 is self dist:0 , second and third 0 is for cell address of first cell
-        dist[0][0]=1;
-
-        while (!q.isEmpty()) {
-            int dis = q.peek().first;
-            int r = q.peek().second;
-            int c = q.peek().third;
+        Queue<Tuple>q=new LinkedList<>();
+        q.add(new Tuple(1,0,0));
+        int dr[]={-1,-1,-1,0,0,1,1,1};
+        int dc[]={-1,0,1,-1,1,-1,0,1};
+        
+        while(!q.isEmpty()){
+            int dis=q.peek().first;
+            int r=q.peek().second;
+            int c=q.peek().third;
             q.remove();
+            if(r==n-1 && c==n-1)return dis;
+            for(int i=0;i<8;i++){
+                int newR=r+dr[i];
+                int newC=c+dc[i];
 
-            int dr[] = { -1, -1, -1, 0, +1, +1, +1, 0 };
-            int dc[] = { -1, 0, +1, +1, +1, 0, -1, -1 };
-
-            for (int i = 0; i < 8; i++) {
-                int newR = r + dr[i];
-                int newC = c + dc[i];
-                if (newR >= 0 && newR < n && newC >= 0 && newC < n && grid[newR][newC] == 0) {
-                    if (dis + 1 < dist[newR][newC]) {
-                        dist[newR][newC] = dis + 1;
-
-                        if (newR == n - 1 && newC == n - 1)return dis+1;
-
-                            q.add(new Tuple(dis + 1, newR, newC));
+                if(newR>=0 && newR<n && newC>=0 && newC<n && grid[newR][newC]==0 ){
+                    if(dis+1<dist[newR][newC]){
+                        dist[newR][newC]=dis+1;
+                        //if(newR==n-1 && newC==n-1)return dis+1;
+                        q.add(new Tuple(dis+1,newR,newC));
+                        
                     }
                 }
             }
         }
         return -1;
-
+        
+        
     }
 }
