230094e120476ba54d46ebc946360b5e48145d29
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-04-15 18:23:39 +0530

    Time: 2 ms (96.49%), Space: 48.5 MB (89.01%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,26 @@
+// class Solution {
+//     public int[] countBits(int n) {
+//         int ans[]=new int[n+1];
+//         for(int i=0;i<=n;i++){
+//             int num=i;
+//             int count=0;
+//             while(num>0){
+//                 if((num & 1)==1)count++;//doing and operaion with the last digit of number
+//                 num=num>>1;//right shift
+//             }
+//             ans[i]=count;
+//         }
+//         return ans; 
+        
+//     }
+// }
 class Solution {
     public int[] countBits(int n) {
         int ans[]=new int[n+1];
-        for(int i=0;i<=n;i++){
-            int num=i;
-            int count=0;
-            while(num>0){
-                if((num & 1)==1)count++;//doing and operaion with the last digit of number
-                num=num>>1;//right shift
-            }
-            ans[i]=count;
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+            ans[i]=ans[i>>1]+(i&1);
         }
-        return ans; 
-        
+        return ans;
     }
 }
