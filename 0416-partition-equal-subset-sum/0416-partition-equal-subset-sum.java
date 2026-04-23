3f546fc2d983cc2832389f46929c0ff5ed7ce182
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-04-23 22:59:51 +0530

    Time: 87 ms (25.46%), Space: 85.9 MB (7.24%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,51 +1,39 @@
 class Solution {
-    // private boolean solve(int nums[],int i,int sum1,Boolean dp[][]){
-    //     if(i<0)return false;
-    //     if(sum1==0)return true;
-    //     if(dp[i][sum1]!=null)return dp[i][sum1];
-    //     boolean notTake=solve(nums,i-1,sum1,dp);
-    //     boolean take=false;
-    //     if(sum1>=nums[i]){
-    //         take=solve(nums,i-1,sum1-nums[i],dp);
-    //     }
-    //     return dp[i][sum1]=take||notTake;
-    // }
-    // public boolean canPartition(int[] nums) {
-    //     int n=nums.length;
-    //     int sum=0;
-    //     for(int i=0;i<n;i++){
-    //         sum+=nums[i];
-    //     }
-    //     Boolean dp[][]=new Boolean[n][sum/2+1];
-    //     if(sum%2!=0)return false;
-    //     return solve(nums,n-1,sum/2,dp);
-    // }
-    //tabulation
-    public boolean canPartition(int[] nums) {
-        int n=nums.length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
+    static int dp[][];
+    private boolean solve(int nums[],int target,int i){
+        if(target==0)return true;
+        if(i==0){
+            if(nums[0]==target)return true;
+            return false;
         }
-        if(sum%2!=0)return false;
-        int target=sum/2;
-        boolean dp[][]=new boolean[n][target+1];
-        //play with base case 
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;//when target is 0 than we can keep true buy not picking any element
+        
+        if(dp[i][target]!=-1)return (dp[i][target]==1);
+        boolean notTake=solve(nums,target,i-1);
+        boolean take=false;
+        if(nums[i]<=target){
+            take=solve(nums,target-nums[i],i-1);
         }
-        if(target>=nums[0])dp[0][nums[0]]=true;
-        for(int i=1;i<n;i++){
-            for(int s=1;s<target+1;s++){
-                boolean notTake=dp[i-1][s];
-                boolean take=false;
-                if(nums[i]<=s){
-                    take=dp[i-1][s-nums[i]];
-                }
-                dp[i][s]=take||notTake;
-            }
+        if(take || notTake){
+            //1 indicate true
+            dp[i][target]=1;
+        }else{
+            //0 indicate false
+            dp[i][target]=0;
         }
-        return dp[n-1][target];
+        return take||notTake;
     }
-
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        if(sum%2==1)return false;
+        //here it will only come if it is not false;
+        int target=sum/2;
+        dp=new int[nums.length][target+1];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);//not computed
+        }
+        return solve(nums,target,nums.length-1);
+    }
 }
