260f4b7544bce6f502c4503a70934e7098817c51
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-04-27 12:19:06 +0530

    Time: 14 ms (13.49%), Space: 46.5 MB (95.75%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Map<Integer,Integer>mpp=new HashMap<>();
-        for(int num:nums){
-            mpp.put(num,mpp.getOrDefault(num,0)+1);
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
         }
-        for(int num:nums){
-            if(mpp.get(num)==1)
-            return num;
+        for(int i=0;i<nums.length;i++){
+            if(mpp.get(nums[i])==1){
+                return nums[i];
+            }
         }
         return -1;
     }
