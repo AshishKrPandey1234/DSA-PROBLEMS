59e56ca6920dc62e25f4286f2242d688afbdaa28
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-05-03 00:23:20 +0530

    Time: 4 ms (59.28%), Space: 42 MB (70.36%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rotatedDigits(int n) {
+        //digit cannot roate are 3,4 and 7
+        int count=0;
+        for(int i=1;i<=n;i++){
+            int num=i;
+            boolean isValid=true;
+            boolean isRotatable=false;
+            while(num>0){
+                int digit=num%10;
+                if(digit==3 || digit==4 || digit==7){
+                    isValid=false;
+                    break;
+                }
+                if(digit==2 || digit== 5 || digit==6 || digit==9){
+                    isRotatable=true;
+                }
+                num/=10;
+            }
+            if(isValid && isRotatable){
+                count++;
+            }
+        }
+        return count;
+    }
+}
