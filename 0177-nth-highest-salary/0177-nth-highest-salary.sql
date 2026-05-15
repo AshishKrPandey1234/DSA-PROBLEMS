175eb2d467e443b50119653552d8a2b848f925ab
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-05-15 14:38:45 +0530

    Time: 468 ms (33.39%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,9 +2,10 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
-      SELECT distinct e1.salary FROM Employee e1
-      WHERE (SELECT COUNT(DISTINCT e2.salary) FROM Employee e2
-      WHERE e2.salary>e1.salary)=N-1
-
+      WITH CTE_NAME AS(SELECT salary, DENSE_RANK()  
+                    OVER (ORDER BY salary DESC) AS dr
+                    FROM Employee
+                )
+      select DISTINCT salary from CTE_NAME where dr=N
   );
 END
