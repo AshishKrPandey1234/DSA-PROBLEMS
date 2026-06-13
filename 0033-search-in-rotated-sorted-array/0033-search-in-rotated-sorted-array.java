194fd60d66bf2a637c9c69bd6d0e01242435d129
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-06-13 18:08:55 +0530

    Time: 0 ms (100%), Space: 43.8 MB (64.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,7 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target) return i;
+        int lo=0, hi=nums.length-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[lo]<=nums[mid]){//left half sorted
+                //check weather target present inside sorted part or not
+                if(nums[lo]<=target && target<=nums[mid]){
+                    hi=mid-1;
+                }else{
+                    lo=mid+1;
+                }
+            }else{//right half is sorted
+                //check weather target is present inside right half or not
+                if(nums[mid]<=target && target<=nums[hi]){
+                    lo=mid+1;
+                }else{
+                    hi=mid-1;
+                }
+
+            }
         }
         return -1;
     }
