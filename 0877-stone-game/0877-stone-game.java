a91b767e8beaf3759224a012e6d5319e8d4a87f9
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-08-11 00:38:55 +0530

    Time: 0 ms (100%), Space: 43.3 MB (41.69%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n=piles.length;
+        long alice=0;
+        long bob=0;
+        int last=piles.length-1;
+        int j=0;
+        boolean flag=true;
+        while(j<last){
+            if(flag==true){//alice will get first chance 
+                if(piles[j]>=piles[last])alice+=piles[j++];
+                else alice+=piles[last--];
+            }
+            else{
+                if(piles[j]>=piles[last])bob+=piles[j++];
+                else bob+=piles[last--];
+            }
+        }
+        if(alice>bob)return true;
+        return false;
     }
 }
