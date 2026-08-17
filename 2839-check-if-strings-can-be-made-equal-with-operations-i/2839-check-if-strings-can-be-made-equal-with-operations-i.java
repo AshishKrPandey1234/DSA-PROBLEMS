0c9e88d9586529df1e7e85bb1ba570b78d3ef897
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-08-18 02:50:06 +0530

    Time: 1 ms (99.88%), Space: 45.4 MB (5.1%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -5,9 +5,9 @@ class Solution {
         for(int i=0;i<4;i++){
             if(s1.charAt(0)==s2.charAt(2) && s1.charAt(2)==s2.charAt(0) && 
                 s1.charAt(1)==s2.charAt(1) && s1.charAt(3)==s2.charAt(3))return true;
-            if(s1.charAt(0)==s2.charAt(2) && s1.charAt(2)==s2.charAt(0) && 
+            else if(s1.charAt(0)==s2.charAt(2) && s1.charAt(2)==s2.charAt(0) && 
                 s1.charAt(1)==s2.charAt(3) && s1.charAt(3)==s2.charAt(1))return true;
-            if(s1.charAt(0)==s2.charAt(0) && s1.charAt(2)==s2.charAt(2) && 
+            else if(s1.charAt(0)==s2.charAt(0) && s1.charAt(2)==s2.charAt(2) && 
                 s1.charAt(1)==s2.charAt(3) && s1.charAt(3)==s2.charAt(1))return true;
         }
         return false;
