43b6b7b20e3cb189c31538c77f145ea7707a472e
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-09-02 13:42:21 +0530

    Time: 19 ms (10.51%), Space: 46.5 MB (25.34%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int len1=s.length();
+        int len2=t.length();
+        if(len1!=len2) return false;
+        HashMap<Character,Integer>mpp=new HashMap<>();
+        for(int i=0;i<len1;i++){
+            char ch=s.charAt(i);
+            if(!mpp.containsKey(ch)){
+                mpp.put(ch,1);
+            }else{
+                mpp.put(ch,mpp.get(ch)+1);
+            }
+        }
+        for(int i=0;i<len2;i++){
+            char ch=t.charAt(i);
+            if(!mpp.containsKey(ch)) return false;
+            int count=mpp.get(ch);
+            if(count==1){
+                mpp.remove(ch);
+            }else{
+                mpp.put(ch,count-1);
+            }
+            
+        }
+        return mpp.isEmpty();   
+    }
+}
