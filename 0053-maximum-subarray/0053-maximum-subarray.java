88c3aedb0892ce53191a25a6a36e42bbf153588a
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-09-22 10:37:38 +0530

    Time: 1 ms (99.93%), Space: 77.4 MB (33.02%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n=nums.length;
-        //store the result (maximum result found so far)
         int res=nums[0];
-
-        //maximum sum of subarray ending at current element
         int maxEnding=nums[0];
-        for(int i=1;i<n;i++){
-            //either extend the previous subarray by adding nums[i] or start new subarray
-            //new subarray will only start when curr element that is nums[i] > maxEnding+nums[i]
+        for(int i=1;i<nums.length;i++){
             maxEnding=Math.max(maxEnding+nums[i],nums[i]);
-            //update result id new subaray is greater and then continue finding bigger maxEnding if not get bigger maxEnding no worries the bigger maxEnding is already stored in res which will be returned later.
-            res=Math.max(res,maxEnding);
+            res=Math.max(maxEnding,res);
         }
         return res;
     }
