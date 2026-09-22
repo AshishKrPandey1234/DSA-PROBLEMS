3e874ad28758b55fdb713d58342aca2dba46373f
AshishKrPandey1234 <akpandey369936@gmail.com>
2026-09-22 10:32:08 +0530

    Time: 1 ms (99.93%), Space: 77.2 MB (66.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //this is kadanes algorithm where we have to keep adding things as soon as at any point by adding someththing res < current elemen from the nums array immediately replce maxEnding with current elment of array now again new subaaray start 
         int n=nums.length;
+        //store the result (maximum result found so far)
         int res=nums[0];
+
+        //maximum sum of subarray ending at current element
         int maxEnding=nums[0];
         for(int i=1;i<n;i++){
-            //Either add with with the previous element or else start with new element if by adding it become lesser than nums[i]  where i current index of nums
+            //either extend the previous subarray by adding nums[i] or start new subarray
+            //new subarray will only start when curr element that is nums[i] > maxEnding+nums[i]
             maxEnding=Math.max(maxEnding+nums[i],nums[i]);
+            //update result id new subaray is greater and then continue finding bigger maxEnding if not get bigger maxEnding no worries the bigger maxEnding is already stored in res which will be returned later.
             res=Math.max(res,maxEnding);
         }
         return res;
